8f1eaa4ad5b75b95194084f1958080ef296f6804
Martin Ruiz <mnruiz2001@gmail.com>
2024-11-25 17:40:24 -0300

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="left">
+            <a href="/" className="logo">
+              <img src="/logo.png" alt="estate-logo" />
+              <span>LamaEstate</span>
+            </a>
+
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <a href="/">Agents</a>
+        </div>
+        <div className="right">
+          <a href="/">Sign In</a>
+          <a href="/" className="register">Sign Up</a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
