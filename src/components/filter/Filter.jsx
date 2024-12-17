ea1425ccefa76c362ad5456bfc90506522b2d608
Martin Ruiz <mnruiz2001@gmail.com>
2024-12-17 15:52:17 -0300

    fix map icons, add use for mixins in scss files

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -31,18 +31,18 @@ const filter = () => {
                   <option value="land">Land</option>
                 </select>
             </div>
-          <div className="item">
+          <div className="item right">
               <label htmlFor="minPrice">Min Price</label>
               <input type="number" id='minPrice' min={0} max={10000000} name='minPrice' placeholder='any' />
             </div>
-            <div className="item">
+          <div className="item right">
               <label htmlFor="maxPrice">Max Price</label>
               <input type="number" id='maxPrice' min={0} max={10000000} name='maxPrice' placeholder='any' />
             </div>
-          <div className="item">
+          <div className="item right">
               <label htmlFor="bedroom">Bedroom</label>
               <input type="text" id='bedroom' name='bedroom' placeholder='any' />
-            </div>
+          </div>
 
             <button>
               <img src="/search.png" alt="search-btn" />
--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -1,10 +1,17 @@
 import "./pin.scss"
 import { Marker, Popup } from 'react-leaflet'
 import { Link } from 'react-router-dom'
+import { Icon } from 'leaflet'
+import marker from '/redpin.png';
+
+const myIcon = new Icon({
+    iconUrl: marker,
+    iconSize: [32,32]
+   })
 
 const Pin = ( {item} ) => {
   return (
-        <Marker position={[item.latitude, item.longitude]}>
+        <Marker position={[item.latitude, item.longitude]} icon={myIcon}>
             <Popup>
                 <div className="popupContainer">
                     <img src={item.img} alt="item-img" />
