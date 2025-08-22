4eeb597c2fdd1a88ceb159e30fbfccf775f586e9
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2025-08-22 17:02:56 -0300

    Add files via upload

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './searchBar.scss'
 import { Link } from 'react-router-dom';
 
-
 const SearchBar = () => {
 
   const [query, setQuery] = useState({
@@ -33,11 +32,11 @@ const SearchBar = () => {
         <input type="text" name='location' placeholder='City Location' />
         <input type="number" name='minPrice' min={0} max={10000000} placeholder='Min Price' />
         <input type="number" name='maxPrice' min={0} max={10000000} placeholder='Max Price' />
-        <button>
-          <Link to={'/list'}>
-            <img src="/search.png" alt="search-btn" />
-          </Link>
-        </button>
+        <Link className='link-css' to={'/list'}>
+          <button>
+              <img src="/search.png" alt="search-btn" />
+          </button>
+        </Link>
       </form>
 
     </div>
